8e6fc49216c08e6fa31b1bc5873d2b9e45f60c89
Daniel Silveira <danieldoamaral10@gmail.com>
2023-08-22 18:25:56 -0300

    chore: axios setup

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default api
